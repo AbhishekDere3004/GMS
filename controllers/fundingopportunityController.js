c97bca5dc3c77c7322d5c8a3d5a0ae25021566d3
Abhishek Dere <abhishekdere3004@gmail.com>
2024-03-22 14:45:20 +0530

    update

--- controllers/fundingopportunityController.js
+++ controllers/fundingopportunityController.js
@@ -72,72 +72,7 @@ const fundingopportunityController = async(req,res) =>{
          } = req.body;
 
        
-            const response = await fundingopportunity.create({Additional_Information_on_Eligibility_c,
-
-               Agency_c,
-               
-               Agency_Name_c,
-               
-               ApplicationDueDate_c,
-               
-               Application_Information__c,
-               
-               Application_Owner__c,
-               
-               ArchiveDate__c,
-               
-               CreatedById,
-               
-               Description__c,
-               
-               Eligibility_and_Selection__c,
-               
-               Eligible_Applicants__c,
-               
-               End_Date__c,
-               
-               Funding_Opportunity_Code__c,
-         
-               Funding_Opportunity_Title_Image__c,
-               
-               Name,
-               
-               Grants_gov_Url__c,
-               
-               InternalUniqueID__c,
-               
-               Is_PreApplication_Available__c,
-               
-               LastModifiedById,
-               
-               Meetings_and_Trainings__c,
-               
-               OwnerId,
-               
-               Program__c,
-               
-               Program_Name__c,
-               
-               Purpose__c,
-               
-               Questionnaire__c,
-               
-               RecordTypeId,
-               
-               Funded_Amount__c,
-               
-               Start_Date__c,
-               
-               Status__c,
-               
-               Total_Allocated_amount__c,
-               
-               Total_Award__c,
-               
-               Total_Quarter__c,
-               
-               Type__c,
-               });
+            const response = await fundingopportunity.create(req.body);
           
   
         console.log(response)
